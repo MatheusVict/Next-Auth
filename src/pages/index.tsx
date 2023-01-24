6a7d47ba4fec7485d4a9c1383e8f1360cda2bc32
MatheusVict <matheusvictorhenrique@gmail.com>
2023-01-24 16:21:53 -0300

    initial configs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,34 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import { useForm } from 'react-hook-form'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const { register, handleSubmit } = useForm();
+
+  function handleSigIn(data: any) {
+    console.log(data);
+    
+  }
+
   return (
     <>
-      <form>
-        <input type="email" name="email" id="email" />
-        <input type="password" name="password" id="password" />
+      <form onSubmit={handleSubmit(handleSigIn)}>
+        <input type="email" 
+        {...register('email')}
+          name="email" id="email"
+          autoComplete='email'
+          placeholder='Email address'
+        /><br />
+        <input 
+          {...register('password')}
+          type="password"
+          name="password"
+          id="password"
+        /><br />
         <button type="submit">Entrar</button>
       </form>
     </>
